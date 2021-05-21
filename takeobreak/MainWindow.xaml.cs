3954b160140c52e6e222d3557c021a2e67012d40
guasam <guasam@codesbiome.com>
2021-05-21 07:46:06 +0530

    Implement StartTimer Logic

--- takeobreak/MainWindow.xaml.cs
+++ takeobreak/MainWindow.xaml.cs
@@ -3,6 +3,8 @@ using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Threading;
 
 namespace takeobreak
 {
@@ -11,6 +13,9 @@ namespace takeobreak
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer timer;
+        private TimeSpan time;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -24,24 +29,77 @@ namespace takeobreak
         private void DurationNumberValidation(object sender, TextCompositionEventArgs e)
         {
             var length = e.Text.Length + DurationNumberTextBox.Text.Length;
+            var selected = DurationNumberTextBox.SelectedText.Length;
             var regex = new Regex("[^0-9]+");
+
             e.Handled = regex.IsMatch(e.Text) || length > 4;
 
-            // PRevent 0 at first place
-            if (e.Handled == false && int.Parse(e.Text) <= 0 && length == 1)
+            // Prevent 0 at first place
+            if (e.Handled == false && int.Parse(e.Text) == 0)
             {
-                e.Handled = true;
+                if (length <= 1 || selected > 0)
+                    e.Handled = true;
             }
+
         }
 
-        private void Duration_Changed(object sender, SelectionChangedEventArgs e)
+        private void StartTimer_Click(object sender, RoutedEventArgs e)
+        {
+            var color = (Color) ColorConverter.ConvertFromString("#FF7FFF8B");
+            MessageTextBlock.Foreground = new SolidColorBrush(color);
+
+            var duration = int.Parse(DurationNumberTextBox.Text);
+            var period = DurationTag();
+
+            // Create Timespam based on duration
+            time = DurationPeriod(duration, period);
+
+            // Initial update of text
+            MessageTextBlock.Text = "Next take-o-break in " + time.ToString("c");
+
+            // Dispatcher Timer
+            timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate
+            {
+                MessageTextBlock.Text = "Next take-o-break in " + time.ToString("c");
+
+                if (time == TimeSpan.Zero)
+                {
+                    timer.Stop();
+
+                    Console.WriteLine("Timer Finished");
+                }
+
+                // Tick the time for a second
+                time = time.Add(TimeSpan.FromSeconds(-1));
+                
+            }, Application.Current.Dispatcher);
+
+        }
+
+        private string DurationTag()
         {
             var item = Duration_ComboBox.SelectedItem as ComboBoxItem;
-            
-            if (item.Tag != null)
+            return item.Tag == null ? "" : item.Tag.ToString();
+        }
+
+        private TimeSpan DurationPeriod(double duration, string period)
+        {
+            TimeSpan span = TimeSpan.Zero;
+
+            if (period == "seconds")
             {
-                MessageTextBlock.Text = item.Tag.ToString();
+                span =  TimeSpan.FromSeconds(duration);
             }
+            else if (period == "minutes")
+            {
+                span = TimeSpan.FromMinutes(duration);
+            }
+            else if (period == "hours")
+            {
+                span = TimeSpan.FromHours(duration);
+            }
+
+            return span;
         }
     }
 }
